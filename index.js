399add6fe6c3b5cdc1657df91ca5813f44c6bb57
alejandraquintero018 <alejandraquintero018@gmail.com>
2022-09-28 21:20:05 -0700

    debugged one of my functions, added a map to one of my functions, have yet to debug the map

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const mysql = require('mysql2');
 const { devNull } = require('os');
 const { brotliDecompress } = require('zlib');
 
-
-
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -17,32 +15,36 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
-console.log('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
-console.log('─██████████████─██████──────────██████─██████████████─██████─────────██████████████─████████──████████─██████████████─██████████████─');
-console.log('─██░░░░░░░░░░██─██░░██████████████░░██─██░░░░░░░░░░██─██░░██─────────██░░░░░░░░░░██─██░░░░██──██░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
-console.log('─██░░██████████─██░░░░░░░░░░░░░░░░░░██─██░░██████░░██─██░░██─────────██░░██████░░██─████░░██──██░░████─██░░██████████─██░░██████████─');
-console.log('─██░░██─────────██░░██████░░██████░░██─██░░██──██░░██─██░░██─────────██░░██──██░░██───██░░░░██░░░░██───██░░██─────────██░░██─────────');
-console.log('─██░░██████████─██░░██──██░░██──██░░██─██░░██████░░██─██░░██─────────██░░██──██░░██───████░░░░░░████───██░░██████████─██░░██████████─');
-console.log('─██░░░░░░░░░░██─██░░██──██░░██──██░░██─██░░░░░░░░░░██─██░░██─────────██░░██──██░░██─────████░░████─────██░░░░░░░░░░██─██░░░░░░░░░░██─');
-console.log('─██░░██████████─██░░██──██████──██░░██─██░░██████████─██░░██─────────██░░██──██░░██───────██░░██───────██░░██████████─██░░██████████─');
-console.log('─██░░██─────────██░░██──────────██░░██─██░░██─────────██░░██─────────██░░██──██░░██───────██░░██───────██░░██─────────██░░██─────────');
-console.log('─██░░██████████─██░░██──────────██░░██─██░░██─────────██░░██████████─██░░██████░░██───────██░░██───────██░░██████████─██░░██████████─');
-console.log('─██░░░░░░░░░░██─██░░██──────────██░░██─██░░██─────────██░░░░░░░░░░██─██░░░░░░░░░░██───────██░░██───────██░░░░░░░░░░██─██░░░░░░░░░░██─');
-console.log('─██████████████─██████──────────██████─██████─────────██████████████─██████████████───────██████───────██████████████─██████████████─');
-console.log('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
-console.log('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
-console.log('─████████████───██████████████─██████████████─██████████████─██████████████───██████████████─██████████████─██████████████─');
-console.log('─██░░░░░░░░████─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██───██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
-console.log('─██░░████░░░░██─██░░██████░░██─██████░░██████─██░░██████░░██─██░░██████░░██───██░░██████░░██─██░░██████████─██░░██████████─');
-console.log('─██░░██──██░░██─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░██──██░░██───██░░██──██░░██─██░░██─────────██░░██─────────');
-console.log('─██░░██──██░░██─██░░██████░░██─────██░░██─────██░░██████░░██─██░░██████░░████─██░░██████░░██─██░░██████████─██░░██████████─');
-console.log('─██░░██──██░░██─██░░░░░░░░░░██─────██░░██─────██░░░░░░░░░░██─██░░░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
-console.log('─██░░██──██░░██─██░░██████░░██─────██░░██─────██░░██████░░██─██░░████████░░██─██░░██████░░██─██████████░░██─██░░██████████─');
-console.log('─██░░██──██░░██─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░██────██░░██─██░░██──██░░██─────────██░░██─██░░██─────────');
-console.log('─██░░████░░░░██─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░████████░░██─██░░██──██░░██─██████████░░██─██░░██████████─');
-console.log('─██░░░░░░░░████─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
-console.log('─████████████───██████──██████─────██████─────██████──██████─████████████████─██████──██████─██████████████─██████████████─');
-console.log('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
+function wordart() {
+    console.log('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
+    console.log('─██████████████─██████──────────██████─██████████████─██████─────────██████████████─████████──████████─██████████████─██████████████─');
+    console.log('─██░░░░░░░░░░██─██░░██████████████░░██─██░░░░░░░░░░██─██░░██─────────██░░░░░░░░░░██─██░░░░██──██░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
+    console.log('─██░░██████████─██░░░░░░░░░░░░░░░░░░██─██░░██████░░██─██░░██─────────██░░██████░░██─████░░██──██░░████─██░░██████████─██░░██████████─');
+    console.log('─██░░██─────────██░░██████░░██████░░██─██░░██──██░░██─██░░██─────────██░░██──██░░██───██░░░░██░░░░██───██░░██─────────██░░██─────────');
+    console.log('─██░░██████████─██░░██──██░░██──██░░██─██░░██████░░██─██░░██─────────██░░██──██░░██───████░░░░░░████───██░░██████████─██░░██████████─');
+    console.log('─██░░░░░░░░░░██─██░░██──██░░██──██░░██─██░░░░░░░░░░██─██░░██─────────██░░██──██░░██─────████░░████─────██░░░░░░░░░░██─██░░░░░░░░░░██─');
+    console.log('─██░░██████████─██░░██──██████──██░░██─██░░██████████─██░░██─────────██░░██──██░░██───────██░░██───────██░░██████████─██░░██████████─');
+    console.log('─██░░██─────────██░░██──────────██░░██─██░░██─────────██░░██─────────██░░██──██░░██───────██░░██───────██░░██─────────██░░██─────────');
+    console.log('─██░░██████████─██░░██──────────██░░██─██░░██─────────██░░██████████─██░░██████░░██───────██░░██───────██░░██████████─██░░██████████─');
+    console.log('─██░░░░░░░░░░██─██░░██──────────██░░██─██░░██─────────██░░░░░░░░░░██─██░░░░░░░░░░██───────██░░██───────██░░░░░░░░░░██─██░░░░░░░░░░██─');
+    console.log('─██████████████─██████──────────██████─██████─────────██████████████─██████████████───────██████───────██████████████─██████████████─');
+    console.log('─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
+    console.log('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
+    console.log('─████████████───██████████████─██████████████─██████████████─██████████████───██████████████─██████████████─██████████████─');
+    console.log('─██░░░░░░░░████─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██───██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
+    console.log('─██░░████░░░░██─██░░██████░░██─██████░░██████─██░░██████░░██─██░░██████░░██───██░░██████░░██─██░░██████████─██░░██████████─');
+    console.log('─██░░██──██░░██─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░██──██░░██───██░░██──██░░██─██░░██─────────██░░██─────────');
+    console.log('─██░░██──██░░██─██░░██████░░██─────██░░██─────██░░██████░░██─██░░██████░░████─██░░██████░░██─██░░██████████─██░░██████████─');
+    console.log('─██░░██──██░░██─██░░░░░░░░░░██─────██░░██─────██░░░░░░░░░░██─██░░░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
+    console.log('─██░░██──██░░██─██░░██████░░██─────██░░██─────██░░██████░░██─██░░████████░░██─██░░██████░░██─██████████░░██─██░░██████████─');
+    console.log('─██░░██──██░░██─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░██────██░░██─██░░██──██░░██─────────██░░██─██░░██─────────');
+    console.log('─██░░████░░░░██─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░████████░░██─██░░██──██░░██─██████████░░██─██░░██████████─');
+    console.log('─██░░░░░░░░████─██░░██──██░░██─────██░░██─────██░░██──██░░██─██░░░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─');
+    console.log('─████████████───██████──██████─────██████─────██████──██████─████████████████─██████──██████─██████████████─██████████████─');
+    console.log('───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────');
+}
+wordart();
+
 
 function appMenu() {
     inquirer.prompt([
@@ -91,6 +93,7 @@ function appMenu() {
                 break;
             case 'Update an Employee Role':
                 //function to update an employee's role 
+                // updateEmployee(); 
                 console.log("Update an employee");
                 break;
             default:
@@ -101,6 +104,29 @@ function appMenu() {
 }
 
 appMenu();
+
+function updateEmployee() {
+    db.query('SELECT * employee') //then map this 
+    const employee
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employee',
+                message: "Which employee's role would you like to update?",
+                choices: employee
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "What would you like the employee's updated role to be?",
+                choices: //["Senior Accountant", "Senior Engineer", "Sales Lead", "Human Resource", "Marketing Lead", "Accountant", "Intern", "Entry Level Engineer"]
+        }
+
+        ]).then((answers)
+        )
+
+}
 
 function createRole() {
     inquirer
@@ -119,7 +145,8 @@ function createRole() {
                 type: 'list',
                 name: 'department',
                 message: 'What department does this role belong to?',
-                choices: ["Engineering", "Sales", "Marketing", "Human Resources", "Finance"]
+                choices: 'SELECT first_name and last_name FROM employee'
+                // ["Engineering", "Sales", "Marketing", "Human Resources", "Finance"]
             }
         ])
         .then(function (answers) {
@@ -127,52 +154,62 @@ function createRole() {
             console.log(answers);
             //query to populate the role table with the role data that the user inputs
             //There is going to be an error here because the answers gives a string for department but we need the department_id which is an INT 
-            //     db.query(`INSERT INTO role (title, salary, department_id) VALUES (${answers})`, function (err, results){
-            //         console.table(results);
-            //     appMenu();
-            // });
+            db.query(`INSERT INTO role (title, salary, department_id) VALUES (${answers})`, function (err, results) {
+                console.table(results);
+                appMenu();
+            });
         });
 }
 
-
+//helper functions 
 function createEmployee() {
-    inquirer
-        .prompt([
+    db.query('SELECT * FROM employee', function (err, results) {
+        const employees = results.map(({ id, first_name, last_name }) => (
             {
-                type: 'input',
-                name: 'firstName',
-                message: 'What is the first name of the Employee?'
-            },
-            {
-                type: 'input',
-                name: 'lastName',
-                message: 'What is the last name of the Employee?'
-            },
-            {
-                type: 'list',
-                name: 'role',
-                message: 'What is the role of the employee?',
-                choices: ["Senior Accountant", "Senior Engineer", "Sales Lead", "Human Resource", "Marketing Lead", "Accountant", "Intern", "Entry Level Engineer"]
+                value: id,
+                name: `${first_name} ${last_name}`
+            }
+        ))
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'firstName',
+                    message: 'What is the first name of the Employee?'
+                },
+                {
+                    type: 'input',
+                    name: 'lastName',
+                    message: 'What is the last name of the Employee?'
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is the role of the employee?',
+                    choices: ["Senior Accountant", "Senior Engineer", "Sales Lead", "Human Resource", "Marketing Lead", "Accountant", "Intern", "Entry Level Engineer"]
 
-            },
-            {
-                type: 'list',
-                name: 'manager',
-                message: 'Who is the employees manager?',
-                choices: ["None", "Marie Curie"]
-            },
-        ])
+                },
+                {
+                    type: 'list',
+                    name: 'manager',
+                    message: 'Who is the employees manager?',
+                    choices: //give a list of all employees, ["None", "Marie Curie"]
+                },
+            ])
+    
         .then(function (answers) {
+            answers.manager
             const { firstName, lastName, role, manager } = answers;
             //query to populate the employee table with the role data that the user inputs 
             //this query will have the same error as the above function
-            // db.query(`INSERT INTO employee (first_name, last_name, manager_id)`, function (err, results) {
-            //     console.log(answers);
-            //     appMenu();
-            // });
+            db.query(`INSERT INTO employee (first_name, last_name, manager_id) VALUES = ?`, (firstName, lastName, role, manager), function (err, results) {
+                console.log(answers);
+                appMenu();
+            });
+            db.query('SELECT * from employee');
         });
-};
 
+};
 
 function createDepartment() {
     inquirer
@@ -183,11 +220,18 @@ function createDepartment() {
                 message: 'What is the name of the department?'
             },
         ])
-        .then(function (answers) {
-            console.log(answers);
+        .then(function (newDept) {
+            console.log(newDept);
             //query to populate the employee table with the role data that the user inputs 
-            //db.query('SHOW ', function (err, results) {
-            //console.log(results);
-            appMenu();
+            db.query('INSERT INTO department (name) VALUES = ?', [newDept.name], function (err, results) {
+                console.table(results);
+                if (err) {
+                    console.log(err);
+                }
+                appMenu();
+            })
         });
 }
+
+
+
